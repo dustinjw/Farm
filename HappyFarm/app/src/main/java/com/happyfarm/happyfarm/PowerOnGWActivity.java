0f8475628e7d8dd77f6a8b6c9cc586f6ed2a4a15
jwlee2 <dustin.jwlee@gmail.com>
2017-09-07 17:55:56 +0900

    add string, login, etc

---
+++ HappyFarm/app/src/main/java/com/happyfarm/happyfarm/PowerOnGWActivity.java
@@ -0,0 +1,13 @@
+package com.happyfarm.happyfarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PowerOnGWActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_power_on_gw);
+    }
+}
